156a1372cda64868d04c05f5adb974b11559353e
Allahshukur <agazade.424@gmail.com>
2021-01-12 21:02:03 +0400

    already created new branch and I add files beacuse changed my branch to new branch

--- src/main/java/az/Allahshukur/App.java
+++ src/main/java/az/Allahshukur/App.java
@@ -2,11 +2,7 @@ package az.Allahshukur;
 
 import az.Allahshukur.Connect.MySQLConnect;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
