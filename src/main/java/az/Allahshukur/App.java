df306d7637a2f30f8e56b6bb2aa41e2a045608f9
Allahshukur <agazade.424@gmail.com>
2021-01-12 20:07:44 +0400

    First Maven

---
+++ src/main/java/az/Allahshukur/App.java
@@ -0,0 +1,15 @@
+package az.Allahshukur;
+
+import az.Allahshukur.Connect.MySQLConnect;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MySQLConnect.connect();
+    }
+}
---
+++ src/main/java/az/Allahshukur/Connect/MySQLConnect.java
@@ -0,0 +1,24 @@
+package az.Allahshukur.Connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MySQLConnect {
+
+    public static void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String host ="sql2.freemysqlhosting.net";
+            String port = "3306";
+            String db = "sql2385489";
+            String userName = "sql2385489";
+            String password = "pQ3!bI2%";
+            String conURL = String.format("jdbc:mysql://%s:%s/%s",host,port,db);
+            Connection con = DriverManager.getConnection(conURL, userName, password);
+            System.out.println("Connected");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
